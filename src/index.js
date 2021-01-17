fa0fe687bf9c0121429540088c69f3e412c47563
djorg@gmail.com <djorg@gmail.com>
2021-01-17 16:32:23 -0500

    server express d prueba

--- src/index.js
+++ src/index.js
@@ -1,60 +1,72 @@
-import 'dotenv/config';
-import cors from 'cors';
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import {IN_PROD, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, SECRET} from './config';
-import typeDefs from './typeDefs';
-import resolvers from './resolvers';
-import jwt from 'jsonwebtoken';
-import schemaDirectives from './directives';
+// import 'dotenv/config';
+// import cors from 'cors';
+// import express from 'express';
+// import { ApolloServer } from 'apollo-server-express';
+// import {IN_PROD, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, SECRET} from './config';
+// import typeDefs from './typeDefs';
+// import resolvers from './resolvers';
+// import jwt from 'jsonwebtoken';
+// import schemaDirectives from './directives';
 
-var mongoose = require('mongoose');
+// var mongoose = require('mongoose');
 
-(async ()=>{
+// (async ()=>{
 
-  try {
+//   try {
 
-    await mongoose.connect(`mongodb+srv://jorge:${DB_PASSWORD}@cluster0.cn88n.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify:false
-    });
-     const app = express();
-     app.use(cors());
+//     await mongoose.connect(`mongodb+srv://jorge:${DB_PASSWORD}@cluster0.cn88n.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useFindAndModify:false
+//     });
+//      const app = express();
+//      app.use(cors());
   
-    //  const addUser = async  (req) => {
+//      const addUser = async  (req) => {
 
-    //   const token = req.headers.authorization
-    //   try {
-    //    const { user } = await jwt.verify(token, SECRET)
-    //    req.user = user
-    //   } catch (error) {
-    //     console.log(error)
-    //   }
-    //   req.next()
-    // }
+//       const token = req.headers.authorization
+//       try {
+//        const { user } = await jwt.verify(token, SECRET)
+//        req.user = user
+//       } catch (error) {
+//         console.log(error)
+//       }
+//       req.next()
+//     }
 
-    // app.use(addUser);
+//     app.use(addUser);
 
-    const server = new ApolloServer({
-      typeDefs,
-      resolvers,
-      schemaDirectives,
-      playground: IN_PROD ? false : {
-        settings:{
-          'request.credentials':'include'
-        }
-      },
-      context:({req, res}) => ({req, res })
-    })
+//     const server = new ApolloServer({
+//       typeDefs,
+//       resolvers,
+//       schemaDirectives,
+//       playground: IN_PROD ? false : {
+//         settings:{
+//           'request.credentials':'include'
+//         }
+//       },
+//       context:({req, res}) => ({req, res })
+//     })
   
-    server.applyMiddleware({ app, path: '/graphql' });
+//     server.applyMiddleware({ app, path: '/graphql' });
   
-    app.listen(process.env.PORT || 8000, () => {
-      console.log('Apollo Server On');
-    })
+//     app.listen(process.env.PORT || 8000, () => {
+//       console.log('Apollo Server On');
+//     })
   
-  } catch (e) {
-    console.error(e)
-  }
-})()
+//   } catch (e) {
+//     console.error(e)
+//   }
+// })()
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+   res.json({jorge: "El salvaje"});
+});
+
+app.listen(port, () => {
+   console.log(`Server is up on port ${port}!`);
+});
